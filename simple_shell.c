18f01fb6a124dad5df293d02977634f814f5d63b
Zihao Zhang <zihaozhang@169-231-164-14.wireless.ucsb.edu>
2019-04-17 16:34:59 -0700

    4.17 4pm

--- simple_shell.c
+++ simple_shell.c
@@ -9,6 +9,10 @@
 #include<fcntl.h>
 #include<unistd.h>
 #include<errno.h>
+#include<signal.h>
+#include <sys/wait.h>
+#include <sys/resource.h>
+
 
 
 #define TRUE 1
@@ -129,6 +133,11 @@ int split_tokens(int numTokens, char tokens[MAXLINE][MAXTOKEN], char commands[50
 }
 
 
+void sigchld_handler(int sig){
+	wait(NULL);
+}
+
+
 void execute_single_command(char tokens[MAXLINE][MAXTOKEN], bool isBackground){
 	int status;
 	char *args[MAXLINE];
@@ -161,9 +170,12 @@ void execute_single_command(char tokens[MAXLINE][MAXTOKEN], bool isBackground){
 	}
 	args[numArgs] = 0;
 
+	signal(SIGCHLD,sigchld_handler);         // catch exit child background process
+
 	pid = fork();
 	if(pid == -1){
-		perror("ERROR: fork failed!\n");
+		fprintf(stderr, "ERROR: %s\n", strerror(errno));
+		_exit(1);
 	}
 
 	else if(pid == 0){
@@ -179,131 +191,135 @@ void execute_single_command(char tokens[MAXLINE][MAXTOKEN], bool isBackground){
 	        close(fd1);
     	}
 		if(execvp(args[0], args) == -1){
-    		printf("ERROR: %s\n", strerror(errno));
+    		fprintf(stderr, "ERROR: %s\n", strerror(errno));
     	}
 
 	}
 	else{
 		// Parent
 		if(isBackground == false){
-			//printf("11111\n");
-			waitpid(-1, &status, 0);
+			waitpid(pid, &status, 0);
 		}else{
-			//printf("22222\n");
-			waitpid(-1, &status, WNOHANG);
+			//waitpid(pid, &status, WNOHANG);
 		}
 	}
 }
 
-// void run_pipe_command(int in, int out, char command[MAXLINE][MAXTOKEN]){
-// 	char *args[MAXLINE];
-// 	int numArgs = 0;
-// 	bool redirectIn = false;
-// 	bool redirectOut = false;
-// 	char inputFile[MAXTOKEN];
-// 	char outputFile[MAXTOKEN];
+void run_pipe_command(int in, int out, char command[MAXLINE][MAXTOKEN]){
+	char *args[MAXLINE];
+	int numArgs = 0;
+	bool redirectIn = false;
+	bool redirectOut = false;
+	char inputFile[MAXTOKEN];
+	char outputFile[MAXTOKEN];
 
-// 	int i = 0;
-// 	while(strcmp(command[i],"end") != 0){
-// 		if( strcmp(command[i], "<") == 0 ){			// redirect input
-// 			redirectIn = true;
-// 			strcpy(inputFile, command[++i]);
-// 			i++;
-// 		}
-// 		else if( strcmp(command[i], ">") == 0 ){		// redirect output
-// 			redirectOut = true;
-// 			strcpy(outputFile, command[++i]);
-// 			i++;
-// 		}
-// 		else{
-// 	        args[numArgs] = command[i];
-// 	        numArgs++;
-// 	        i++;
-// 	    }
-// 	}
-// 	args[numArgs] = 0;
-
-// 	if(in != STD_INPUT){
-// 		dup2(in,STD_INPUT);
-//     	close(in);
-// 	}else{
-// 		if (redirectIn == true){
-// 			int fd0 = open(inputFile, O_RDONLY);
-// 	        dup2(fd0, STD_INPUT);
-// 	        close(fd0);
-//     	}
-// 	}
+	int i = 0;
+	while(strcmp(command[i],"end") != 0){
+		if( strcmp(command[i], "<") == 0 ){			// redirect input
+			redirectIn = true;
+			strcpy(inputFile, command[++i]);
+			i++;
+		}
+		else if( strcmp(command[i], ">") == 0 ){		// redirect output
+			redirectOut = true;
+			strcpy(outputFile, command[++i]);
+			i++;
+		}
+		else{
+	        args[numArgs] = command[i];
+	        numArgs++;
+	        i++;
+	    }
+	}
+	args[numArgs] = 0;
 	
-//     if(out != STD_OUTPUT){
-//     	dup2(out, STD_OUTPUT);
-//     	close(out);
-//     }else{
-//     	if (redirectOut == true){
-// 	    	int fd1 = creat(outputFile, 0644);
-// 	        dup2(fd1, STD_OUTPUT);
-// 	        close(fd1);
-//     	}
-//     }
-
-//     if(execvp(args[0], args) == -1){
-//     	 printf("ERROR: %s\n", strerror(errno));
-//     }
-
-// }
-
-
-// void launch_pipes(int numCommands, char commands[50][MAXLINE][MAXTOKEN], bool isBackground){
-// 	int pid;
-// 	int fd[2];
-// 	int status;
-// 	int in = 0;
-// 	int out = 1;
-
-// 	// first command to the second last command
-// 	for(int i = 0; i < numCommands - 1; i++){
-// 		pipe(fd);
-// 		pid = fork();
-// 		if(pid == -1){
-// 			printf("ERROR: %s\n", strerror(errno));
-// 		}else if(pid == 0){
-// 			// Child
-// 			close(fd[0]);
-// 			run_pipe_command(in, fd[1], commands[i]);
+	if(in != STD_INPUT){
+		dup2(in,STD_INPUT);
+    	close(in);
+	}else{
+		if (redirectIn == true){
+			int fd0 = open(inputFile, O_RDONLY);
+	        dup2(fd0, STD_INPUT);
+	        close(fd0);
+    	}
+	}
 	
-// 		}else{
-// 			// Parent
-// 			close(fd[1]);
-// 			waitpid(-1, &status, 0);
-// 			if(in != STD_INPUT){
-// 				close(in);
-// 			}
-// 			in = fd[0];
-// 		}
-// 	}
+    if(out != STD_OUTPUT){
+    	dup2(out, STD_OUTPUT);
+    	close(out);
+    }else{
+    	if (redirectOut == true){
+	    	int fd1 = creat(outputFile, 0644);
+	        dup2(fd1, STD_OUTPUT);
+	        close(fd1);
+    	}
+    }
 
-// 	pid = fork();
-// 	if(pid == -1){	
-// 	  	printf("ERROR: %s\n", strerror(errno));
-// 	}else if(pid == 0){
-// 	  	//Child
-// 		run_pipe_command(in, out, commands[numCommands-1]);
-// 	}else{
-// 	  	// Parent
+    if(execvp(args[0], args) == -1){
+    	fprintf(stderr, "ERROR: %s\n", strerror(errno));
+    	_exit(1);
+    }
 
-// 	  	if(isBackground == false){
-// 	  		//printf("33333\n");
-// 		  	waitpid(-1, &status, 0);
-// 		}else{
-// 			//printf("44444\n");
-// 			waitpid(-1, &status, WNOHANG);
-// 		}
+}
 
-// 		if(in != STD_INPUT){
-// 			close(in);
-// 		}
-// 	}
 
-// }
+void launch_pipes(int numCommands, char commands[50][MAXLINE][MAXTOKEN], bool isBackground){
+	int pid;
+	int fd[2];
+	int status;
+	int in = 0;
+	int out = 1;
+
+	signal(SIGCHLD,sigchld_handler);
+
+	// first command to the second last command
+	for(int i = 0; i < numCommands - 1; i++){
+		pipe(fd);
+		pid = fork();
+		if(pid == -1){
+			fprintf(stderr, "ERROR: %s\n", strerror(errno));
+		}else if(pid == 0){
+			// Child
+			close(fd[0]);
+			run_pipe_command(in, fd[1], commands[i]);
+	
+		}else{
+			// Parent
+			close(fd[1]);
+
+			if (isBackground == false)
+			{
+				waitpid(pid, &status, 0);
+			}
+
+			if(in != STD_INPUT){
+				close(in);
+			}
+			in = fd[0];
+		}
+	}
+
+	pid = fork();
+	if(pid == -1){	
+	  	fprintf(stderr, "ERROR: %s\n", strerror(errno));
+	}else if(pid == 0){
+	  	//Child
+		run_pipe_command(in, out, commands[numCommands-1]);
+	}else{
+	  	// Parent
+
+	  	if(isBackground == false){
+		  	waitpid(pid, &status, 0);
+		}else{
+			//waitpid(-1, &status, WNOHANG);
+		}
+
+		if(in != STD_INPUT){
+			close(in);
+		}
+	}
+
+}
 
 
 int main(){
@@ -315,23 +331,25 @@ int main(){
 	int numCommands = 0;
 	bool isBackground = false;
 	
-	while(!feof(stdin)){
+	while(true){
 
+		fflush(stdout);
+		fflush(stderr);
 		type_prompt(); 
 
 		memset(line,0,strlen(line));
 		get_line(line);
 		if(feof(stdin)){
 			exit(1);
-		}
-		//printf("Line : %s \n", line);			// for testing
+		}	
+		//printf("Line : %s \n", line);								// for testing
 
 
 		for(int i = 0; i < numTokens; i++){
 			memset(tokens[i],0,strlen(tokens[i]));
 		}
 		numTokens = split_line(line, tokens);
-		//printf("Tokens : %d \n", numTokens);				// for testing
+		//printf("Tokens : %d \n", numTokens);						// for testing
 		//test_print_tokens(numTokens, tokens);
 
 		for(int i = 0; i < numCommands; i++){
@@ -347,18 +365,18 @@ int main(){
 		if(strcmp(tokens[numTokens - 1], "&") == 0){
 			isBackground = true;
 		}
-		//printf("isBackground : %d\n", isBackground);		// for testing
+		//printf("isBackground : %d\n", isBackground);				// for testing
 
 		
 		if(numCommands == 0){
 			// do nothing
 		}
 		else if(numCommands == 1){
-			//printf("excuting single command\n");			// for testing
+			//printf("excuting single command\n");					// for testing
 			execute_single_command(commands[0], isBackground);
 		}else{
-			//printf("launching pipe\n");
-			//launch_pipes(numCommands, commands, isBackground);
+			//printf("launching pipe\n");							// for testing
+			launch_pipes(numCommands, commands, isBackground);
 		}
 		
 	}
